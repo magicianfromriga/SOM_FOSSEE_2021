78de17faa9152b8c85bc90f75e17cc414587e299
magicianofriga <58387591+magicianofriga@users.noreply.github.com>
2021-05-21 12:06:19 +0530

    Update Data_Distance.R
    Updating to use data.table

--- Tanmay/Data_Distance.R
+++ Tanmay/Data_Distance.R
@@ -1,7 +1,7 @@
 # 1) Reading the data and scaling it
 data <- read.csv("binary.csv", header = T)
 X <- scale(data[, -1])
-data <- as.data.frame(X)
+data <- data.table(X)
 
 # 2) SSD function
 ssd <- function(x, y) {
@@ -10,10 +10,11 @@ ssd <- function(x, y) {
 }
 
 # 3) Creating a 4*4 grid for the SOM
-crt_gr <- function(x) {
-  ret <- matrix(data = rnorm(x * 3), nrow = x, ncol = 3)
-  return(ret)
+crt_gr <- function(n,p) {
+  ret <- matrix(data = rnorm(n * p), nrow = n, ncol = p)
+  return(as.data.table(ret))
 }
 
 set.seed(222)
-grid <- crt_gr(16)
+grid <- crt_gr(16,3)
+
