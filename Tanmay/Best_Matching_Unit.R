3d318153611cef34f7f2af85b130240e380d5f3f
magicianofriga <58387591+magicianofriga@users.noreply.github.com>
2021-05-24 12:07:16 +0530

    Adding the Vectorisation implementation.

--- Tanmay/Best_Matching_Unit.R
+++ Tanmay/Best_Matching_Unit.R
@@ -1,3 +1,4 @@
+library(profvis)
 # Function to return winning neuron
 BMU <- function(x, gr) { #x is a single row of data and gr is the grid
   dst <- 0
@@ -13,3 +14,24 @@ BMU <- function(x, gr) { #x is a single row of data and gr is the grid
   }
   return(min_ind)
 }
+#x is a single row of data and gr is the grid
+BMU_Vectorised <- function(x, gr) { 
+temp=as.vector(unlist(x))
+dist_mtrx=rowSums(sweep(grid,2,temp)^2)
+min_ind=which.min(dist_mtrx)
+return (min_ind)
+}
+
+profvis(
+for(i in 1:400)
+{
+  BMU(data[i,],grid)
+}
+)
+profvis(
+  for(i in 1:400)
+  {
+    BMU_Vectorised(data[i,],grid)
+  }
+)
+
