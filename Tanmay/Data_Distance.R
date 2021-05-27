b73ac7864f39ce873cfd60cf7d0ba39f91ec3852
magicianofriga <58387591+magicianofriga@users.noreply.github.com>
2021-05-27 09:55:37 +0530

    Added better comments and separated sections.

--- Tanmay/Data_Distance.R
+++ Tanmay/Data_Distance.R
@@ -1,20 +1,43 @@
+#This is the R Script for loading the data and creating the grid.
+
+#-------------------------------------------------------------------------------
+
+#Loading the required library.
+library(data.table)
+
+#-------------------------------------------------------------------------------
+
 # 1) Reading the data and scaling it
+
 data <- read.csv("binary.csv", header = T)
 X <- scale(data[, -1])
 data <- data.table(X)
 
-# 2) SSD function
+#-------------------------------------------------------------------------------
+
+# 2) A function to return the sum of squared distance between x and y.
+
 ssd <- function(x, y) {
   ret <- sum((x - y)^2)
   return(ret)
 }
 
-# 3) Creating a 4*4 grid for the SOM
+#-------------------------------------------------------------------------------
+
+# 3) Function to create a SOM grid.
+# n is the number of neurons.
+# p is the number of columns in the original dataframe.
+# data table is used for faster computation.
+
 crt_gr <- function(n,p) {
   ret <- matrix(data = rnorm(n * p), nrow = n, ncol = p)
   return(as.data.table(ret))
 }
 
+#-------------------------------------------------------------------------------
+
+#Creating a 4*4 grid using the function defined above.
 set.seed(222)
 grid <- crt_gr(16,3)
 
+#-------------------------------------------------------------------------------
