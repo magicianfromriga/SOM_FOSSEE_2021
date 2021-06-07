cd6c084f778e8d9e0740dfb945d5a3f89240ca75
magicianofriga <58387591+magicianofriga@users.noreply.github.com>
2021-06-07 13:55:21 +0530

    Call crt_gr() here
    Added create grid function here so that it's easier to make edits

--- Tanmay/Matrix Implementation/combination(Matrix).R
+++ Tanmay/Matrix Implementation/combination(Matrix).R
@@ -3,6 +3,12 @@ source("Data_Distance(Matrix).R")
 source("Decaying(Matrix).R")
 source("BMU(Matrix).R")
 source("SOM_Fn(Matrix).R")
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Creating a 5*5 grid using the function defined above.
+set.seed(222)
+grid <- crt_gr(5,5,3)
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 gridSOM=SOM(data,grid) #Using previously written code to find the Self Organising Map
 profvis(
   {
